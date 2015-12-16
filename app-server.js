23bf2f1de7bc6c0de77a3bb6f6b21fcae69b671a
Josh Nykamp <josh.nykamp@gmail.com>
2015-12-16 16:45:14 +0000

    login is working

--- app-server.js
+++ app-server.js
@@ -2,16 +2,43 @@ var express = require('express');
 var dbConfig = require('./db.js');
 var mongoose = require('mongoose');
 var passport = require("passport");
-var expressSession = require("express-session");
+var LocalStrategy = require('passport-local').Strategy;
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var routes = require('./routes/index');
+var path = require('path');
+
 var app = express();
 
-mongoose.connect(dbConfig.url);
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
 app.use(express.static('./public'));
 app.use(express.static('./node_modules/bootstrap/dist'));
-app.use(expressSession({secret: 'reTAHQwJpCa1LMT'}));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(require('express-session')({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+}));
 app.use(passport.initialize());
 app.use(passport.session());
+app.use('/', routes);
+
+var Account = require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
+
+mongoose.connect(dbConfig.url);
+
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
 
 var server = app.listen(process.env.PORT, process.env.IP);
 var io = require("socket.io").listen(server);
--- public/bundle.js
+++ public/bundle.js
@@ -26870,7 +26870,7 @@
 	var LatestUsers = React.createClass({
 	    displayName: 'LatestUsers',
 	    getDefaultProps: function getDefaultProps() {
-	        //Do an ajax call for that past two users that signed up.
+	        //Do an ajax call for that past two users that signed up if there is no connection.
 	        return {
 	            users: [{ id: 1, name: 'Josh', city: 'Lakewood' }, { id: 2, name: 'Bill', city: 'Denver' }],
 	            status: 'test'

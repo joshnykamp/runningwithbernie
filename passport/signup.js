464baf37e6f8b94f361ee3d47590160bed548d75
Josh Nykamp <josh.nykamp@gmail.com>
2016-01-04 02:29:55 +0000

    adding any mossing files

---
+++ passport/signup.js
@@ -0,0 +1,69 @@
+var LocalStrategy   = require('passport-local').Strategy;
+var User = require('../models/user');
+var bCrypt = require('bcrypt');
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+module.exports = function(passport){
+
+	passport.use('signup', new LocalStrategy({
+            passReqToCallback : true // allows us to pass back the entire request to the callback
+        },
+        function(req, username, password, done) {
+
+            findOrCreateUser = function(){
+                // find a user in Mongo with provided username
+                User.findOne({ 'username' :  username }, function(err, user) {
+                    // In case of any error, return using the done method
+                    if (err){
+                        console.log('Error in SignUp: '+err);
+                        return done(err);
+                    }
+                    // already exists
+                    if (user) {
+                        console.log('User already exists with username: '+username);
+                        return done(null, false);
+                    } else {
+                        // if there is no user with that email
+                        // create the user
+                        var newUser = new User();
+
+                        // set the user's local credentials
+                        newUser.username = username;
+                        newUser.password = createHash(password);
+                        newUser.zipcode = req.param('zipcode');
+                        //newUser.firstName = req.param('firstName');
+                        //newUser.lastName = req.param('lastName');
+
+                        // save the user
+                        newUser.save(function(err) {
+                            if (err){
+                                console.log('Error in Saving user: '+err);  
+                                throw err;  
+                            }
+                            
+                            io.on('connection', function(socket){
+                                socket.on('new user', function(newUser){
+                                    io.emit('new user', newUser);
+                                });
+                            });
+                            
+                            console.log('User Registration succesful');    
+                            return done(null, newUser);
+                        });
+                    }
+                });
+            };
+            // Delay the execution of findOrCreateUser and execute the method
+            // in the next tick of the event loop
+            process.nextTick(findOrCreateUser);
+        })
+    );
+
+    // Generates hash using bCrypt
+    var createHash = function(password){
+        return bCrypt.hashSync(password, bCrypt.genSaltSync(10), null);
+    }
+
+}
